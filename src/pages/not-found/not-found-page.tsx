481c20aff15c8ef8c4f8c47e9e9eb70629d926a1
David Alsh <alshdavid@gmail.com>
2026-01-19 17:22:56 +1100

    domain

--- src/pages/not-found/not-found-page.tsx
+++ src/pages/not-found/not-found-page.tsx
@@ -1,6 +1,15 @@
+import { useRouter } from "../../platform/router/preact.tsx";
 import "./not-found-page.css";
 import { Fragment, h } from "preact";
 
 export function NotFoundPage() {
-  return <Fragment>404 Not Found</Fragment>;
+  const router = useRouter();
+  return (
+    <Fragment>
+      <div>404 Not Found</div>
+      <div>
+        <pre>{JSON.stringify(router.req, null, 2)}</pre>
+      </div>
+    </Fragment>
+  );
 }
